44e005a32fbfc48d1689bb14b305f73a5f59fcfe
Dalrsk <diegolopezramos99@gmail.com>
2025-06-24 20:45:50 -0400

    Avance 1

---
+++ seccion1.js
@@ -0,0 +1,32 @@
+//esto es un comentario en linea
+/*
+comentario de multiple lineas
+*/
+
+//variables
+/*
+las variables son espacios de memoria que almacenan un valor
+*/
+// Declaracion de una variable
+var nombre ="Diego"; // variable de tipo string
+console.log(nombre); // imprime el valor de la variable nombre "Diego"
+// Reasignacion de una variable
+nombre ="Pedro"; // Reasignacion
+console.log(nombre); // imprime "Pedro" 
+
+
+// Tipos de datos
+let number = 42; // Numero
+let float = 3.14; // Numero de punto flotante
+let string = "hola, mundo" // Cadena de texto
+let bolean = true; // Booleano
+let array = ["pera", "manzana", "uvas"]; // Array (lista)
+console.log(array[0]); // imprime "pera" (primer elemento del array)
+let nulo = null; // Nulo (sin valor)
+let indefinido; // indefinido (sin asignar)
+let objeto = { nombre: "Diego", edad: 30 }; // Objeto
+console.log(objeto); // Imprime { nombre: "Diego", edad: 30 }
+console.log (objeto.nombre); // Imprime "Diego" (acceso a una propiedad del objeto)
+
+let suma = 5 + 3; // Suma
+let resta = "a10" + "2"; // Resta
